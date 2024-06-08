88c32d6b93449da8efcdda1cd351f004c5da54e7
glencorahaskins <113258428+glencorahaskins@users.noreply.github.com>
2024-06-08 11:00:46 -0500

    Update mountainPlainsIframe.js

--- mountainPlainsIframe.js
+++ mountainPlainsIframe.js
@@ -2,29 +2,28 @@ class MountainPlainsIframe extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `<style>
+            :host {
+                display: block;
+                width: 100%;
+                height: 100%;
+            }
+            iframe {
+                width: 100%;
+                height: 100%;
+                border: none;
+            }
+        </style>`;
     }
 
     connectedCallback() {
         this.loadContent();
     }
 
-    async loadContent() {
-        const url = 'https://glencorahaskins.github.io/bbb/mountain_plains_iframe.html';
-        try {
-            const response = await fetch(url);
-            if (!response.ok) {
-                throw new Error(`Network response was not ok: ${response.statusText}`);
-            }
-            const html = await response.text();
-            console.log('Fetched HTML content:', html);
-
-            // Insert content into the shadow DOM
-            this.shadowRoot.innerHTML = html;
-
-        } catch (error) {
-            console.error('Failed to fetch content:', error);
-            this.shadowRoot.innerHTML = '<p>Failed to load content.</p>';
-        }
+    loadContent() {
+        const iframe = document.createElement('iframe');
+        iframe.src = 'https://glencorahaskins.github.io/bbb/mountain_plains_iframe.html';
+        this.shadowRoot.appendChild(iframe);
     }
 }
 
